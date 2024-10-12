daa40c489eff164d421231e844897f1fb114f454
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:19:24 +0700

    Create for_loop.dart

---
+++ for_loop.dart
@@ -0,0 +1,30 @@
+//INI ADALAH CONTOH FOR LOOP TANPA BATAS
+// void main(){
+//   for(;;){
+//     print('ini tanpa henti');
+//   }
+// }
+
+//INI ADALAH CONTOH FOR LOOP DENGAN MEMAKAI KONDISI
+// void main(){
+//   var jumlah = 1;
+//   for(;jumlah<=10;){
+//     print('$jumlah. hai ges');
+//     jumlah++;
+//   }
+// }
+
+//INI ADALAH CONTOH FOR LOOP DENGAN INIT STATEMENT
+// void main(){
+//   for(var jumlah = 1;jumlah <= 10;){
+//     print('$jumlah. mantap');
+//     jumlah++;
+//   }
+// }
+
+// INI ADALAH CONTOH FOR LOOP DENGAN POST STATEMENT
+// void main(){
+//   for(var jumlah = 1;jumlah <= 10;jumlah++){
+//     print('$jumlah. haloooo');
+//   }
+// }
